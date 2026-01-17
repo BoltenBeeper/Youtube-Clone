3aebade64c0ff28a1bfb7c426d1f0dff87d7f057
BoltenCoder <moltencreeper25@gmail.com>
2026-01-16 23:52:07 -0600

    Added extra code to catagories scroller to make scroll buttons appear/disappear at end points. Adjested large portions of styles to be more accurate to source material. Included media queries to adjust for dynamic sizing.

--- scripts/catagory_scroll.js
+++ scripts/catagory_scroll.js
@@ -1,8 +1,40 @@
 function scrollByAmount(scrollAmount, scrollableDivId) {
   let scrollableDiv = document.getElementById(scrollableDivId);
-  console.log(scrollableDiv)
-  scrollableDiv.scrollBy({
-    left: scrollAmount,
-    behavior: "smooth"
-  })
-}
+  scrollableDiv.scrollBy({left: scrollAmount, behavior: "smooth"})
+  leftArrow = document.getElementById("left-arrow-icon");
+  rightArrow = document.getElementById("right-arrow-icon");
+
+  // Shows and hide the scroll buttons when reaching the end of the scrolling container
+  if (scrollAmount < 0 && scrollableDiv.scrollLeft <= Math.abs(scrollAmount)) { // If scrolling left and scrolling a greater amount than there is left to scroll
+    leftArrow.classList.add("hidden");
+  } else {
+    leftArrow.classList.remove("hidden");
+  }
+
+  const maxScrollWidth = scrollableDiv.scrollWidth - scrollableDiv.clientWidth;
+  if (scrollAmount > 0 && scrollableDiv.scrollLeft >= (maxScrollWidth - Math.abs(scrollAmount))) { // If scrolling right and scrolling a greater amount than there is left to scroll
+    rightArrow.classList.add("hidden");
+  } else {
+    rightArrow.classList.remove("hidden");
+  }
+};
+
+function unhighlightAllElements(elementClass) {
+  elementList = document.querySelectorAll(elementClass);
+  elementList.forEach(element => {
+    element.classList.remove("selected")
+  });
+};
+
+function highlightElement(element) {
+  unhighlightAllElements(".catagory")
+  element.classList.add("selected")
+};
+
+catagoryButtons = document.querySelectorAll(".catagory");
+catagoryButtons.forEach(catagoryButton => {
+  catagoryButton.addEventListener("mousedown", (event) => {
+    event.preventDefault();
+    highlightElement(catagoryButton);
+  });
+});
